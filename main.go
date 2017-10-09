5a16adaf34bff9bab06f5b35c5309495c50c481a
ryonakao <rodriguez.nak@gmail.com>
2017-10-09 14:00:43 +0900

    Add function handler

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
 )
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
+}
 
 func main() {
 	var port = flag.String("p", "8080", "Set Port")
 	flag.Parse()
-	http.HandleFunc("/", request)
+	http.HandleFunc("/", handler)
 
 	log.Println("The server is now ready to accept connections on port " + *port)
 
